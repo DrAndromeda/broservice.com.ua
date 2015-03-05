63e6cb1b6b5125123490a3e2f58d63c8c015ac18
DrAndromeda <kim_55@bigmir.net>
2015-03-05 14:18:59 +0200

    local host

---
+++ assets/cache/docid_1.pageCache.php
@@ -0,0 +1,150 @@
+<?php die('Unauthorized access.'); ?>a:41:{s:2:"id";s:1:"1";s:4:"type";s:8:"document";s:11:"contentType";s:9:"text/html";s:9:"pagetitle";s:17:"О сервисе";s:9:"longtitle";s:17:"О сервисе";s:11:"description";s:1:"/";s:5:"alias";s:9:"o-servise";s:15:"link_attributes";s:0:"";s:9:"published";s:1:"1";s:8:"pub_date";s:1:"0";s:10:"unpub_date";s:1:"0";s:6:"parent";s:1:"0";s:8:"isfolder";s:1:"1";s:9:"introtext";s:0:"";s:7:"content";s:870:"<p><span>Раньше, когда все верстали с использованием таблиц, создать колонки одинаковой высоты было очень просто. &nbsp;Достаточно создать таблицу, например, с 3мя колонками и все они автоматически будут иметь одинаковую высоту. &nbsp;Но в блочной верстке не все так просто.</span><br /><span>В этой статье я расскажу вам о некоторых способах создания колонок равной высоты и о совместимости этих методов с браузерами (включая IE6). Все эти способы описывают создание 3х колоночного макета.</span></p>";s:8:"richtext";s:1:"1";s:8:"template";s:1:"3";s:9:"menuindex";s:1:"1";s:10:"searchable";s:1:"1";s:9:"cacheable";s:1:"1";s:9:"createdby";s:1:"1";s:9:"createdon";s:10:"1130304721";s:8:"editedby";s:1:"1";s:8:"editedon";s:10:"1424618611";s:7:"deleted";s:1:"0";s:9:"deletedon";s:1:"0";s:9:"deletedby";s:1:"0";s:11:"publishedon";s:10:"1423561336";s:11:"publishedby";s:1:"1";s:9:"menutitle";s:17:"О сервисе";s:7:"donthit";s:1:"0";s:11:"haskeywords";s:1:"0";s:11:"hasmetatags";s:1:"0";s:10:"privateweb";s:1:"0";s:10:"privatemgr";s:1:"0";s:13:"content_dispo";s:1:"0";s:8:"hidemenu";s:1:"0";s:13:"alias_visible";s:1:"1";s:10:"column_img";a:5:{i:0;s:10:"column_img";i:1;s:0:"";i:2;s:0:"";i:3;s:0:"";i:4;s:5:"image";}s:8:"keywords";a:5:{i:0;s:8:"keywords";i:1;s:0:"";i:2;s:0:"";i:3;s:0:"";i:4;s:4:"text";}s:17:"__MODxDocGroups__";s:0:"";}<!--__MODxCacheSpliter__--><!DOCTYPE html>
+<html lang="en">
+	<head>
+		
+    <title>My MODX Site | О сервисе</title>
+	<meta name="viewport" content="width=device-width">	
+		
+    <meta charset="UTF-8">
+		<base href="http://localhost/broservice.com.ua/" />
+		<meta name="keywords" content="" />
+		<meta name="description" content="/" />
+    <link href="assets/templates/Free%20Website%20Template%20Security%20Project/css/style.css"rel="stylesheet" type="text/css" media="screen" href="css/reset.css">
+    <link href="assets/templates/Free%20Website%20Template%20Security%20Project/css/style.css"rel="stylesheet" type="text/css" media="screen" href="css/style.css">
+		<script src="assets/templates/Free%20Website%20Template%20Security%20Project/js/jquery-1.7.min.js"></script>
+		<script src="assets/templates/Free%20Website%20Template%20Security%20Project/js/jquery.easing.1.3.js"></script>
+		<script src="assets/templates/Free%20Website%20Template%20Security%20Project/js/FF-cash.js"></script>
+		
+		<link rel="stylesheet" type="text/css" href="assets/themes/styzo/css/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="assets/templates/Free\ Website\ Template\ Security\ Project/css/bootstrap-responsive.css" />
+		<link rel="stylesheet" type="text/css" href="assets/templates/Free\ Website\ Template\ Security\ Project/css/jquery.vegas.css" />
+		<link rel="stylesheet" type="text/css" href="assets/templates/Free\ Website\ Template\ Security\ Project/css/cubeportfolio.css">
+		<link rel="stylesheet" type="text/css" href="assets/templates/Free\ Website\ Template\ Security\ Project/css/animate.css" />
+		<link rel="stylesheet" type="text/css" href="assets/templates/Free\ Website\ Template\ Security\ Project/css/style.css" />
+		<link href="index-id=42.php" rel="stylesheet">
+	<!--[if lt IE 8]>
+       <div style=' clear: both; text-align:center; position: relative;'>
+         <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+           <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+        </a>
+      </div>
+    <![endif]-->
+    <!--[if lt IE 9]>
+   		<script type="text/javascript" src="js/html5.js"></script>
+    	<link rel="stylesheet" type="text/css" media="screen" href="css/ie.css">
+	<![endif]-->
+	
+</head>
+<body>
+	<header>
+	<style type="text/css">
+    b:first-child {
+     color: #d3d3d3; /* Красный цвет текста */
+    }
+  </style>
+  	<div class="main">
+      <div class="wrap">
+		  <div class="linkblock" onclick="location.href='http://broservice.com.ua/';"><a><b>Bro</b>Service.com.ua</a>
+				<div class="logo">	
+				</div>
+			</div>
+					
+				
+		  <div class="slogan">Лучший сервис в Броварах! <br> тел. 093-270-46-96 </div>
+		  
+          <div class="tooltips">
+			  <a href="#"><img src="assets/templates/Free%20Website%20Template%20Security%20Project/images/icon-1.png" alt=""></a><a href="#"><img src="assets/templates/Free%20Website%20Template%20Security%20Project/images/icon-2.png" alt=""></a><a href="#"><img src="assets/templates/Free%20Website%20Template%20Security%20Project/images/icon-3.png" alt=""></a>
+          </div>
+      </div>
+      
+		  <div class="nav-shadow">
+      	<div>
+          <nav>
+              <ul class="menu">
+				  [!Wayfinder? &startId=`0` &level=`2`!] 
+                  
+              </ul>    
+          </nav>
+         </div> 
+      </div>
+    </div> 
+
+
+  
+
+
+
+  
+
+		
+		
+		
+		
+
+     
+    <div class="header-content">
+      <div class="wrap main">
+		  
+      	[!Ditto? &parents=`1` &tpl=`column_tpl` &tplLast=`column_tpl_last` &randomize=`1` &display=`3`!]
+        
+      </div>
+    </div>
+  </header>
+	
+	<section id="content" class="content-subpages">
+      <div>
+      	<div class="wrap">
+            <h1 class="p2">О сервисе  </h1>
+			
+			<!--////////// services SECTION //////////-->
+
+			<section id="section-9" class="services page">
+		   <div id="services" class="full-width">
+		        <div class="container">
+			        <div class="row">
+					<div class="services-area">
+						
+							<div class="circle">
+							  <div class="icon">
+								  <img src="assets/icons/settings3.png" alt="">
+							  </div>
+							</div>
+      
+ 
+         <div class="circle1">
+               <div class="icon">
+ <img src="assets/icons/arrow12.png" alt="">
+ </div>
+</div></div>
+
+							
+		</div>
+
+   <div class="circle2" >
+ <div class="icon">
+ <img src="assets/icons/flashon.png" alt="">
+ </div>
+							</div>
+							
+		</div>
+						</div>
+			
+		
+</section>
+			<p><span>Раньше, когда все верстали с использованием таблиц, создать колонки одинаковой высоты было очень просто. &nbsp;Достаточно создать таблицу, например, с 3мя колонками и все они автоматически будут иметь одинаковую высоту. &nbsp;Но в блочной верстке не все так просто.</span><br /><span>В этой статье я расскажу вам о некоторых способах создания колонок равной высоты и о совместимости этих методов с браузерами (включая IE6). Все эти способы описывают создание 3х колоночного макета.</span></p>
+        </div>
+      </div>
+  </section> 
+<!--==============================header=================================-->
+  <div class="ic">More Website Templates @ TemplateMonster.com - March 12, 2012!</div>
+<!--==============================content================================-->
+	<footer>
+	<p> BroService.com.ua ·  2015 · Все права защищены.</p> 
+      <p>Телефон: 067 559 6580 &nbsp; Email: <a href="#" class="link">info@broservice.com.ua</a></p>
+	<a>Партнеры</a> : <a rel="nofollow" href="http://crystalwater.kiev.ua/" class="link" target="_blank">crystalwater.kiev.ua</a>
+
+	 <a rel="nofollow" href="http://AdFoto.com.ua" class="link" target="_blank">AdFoto.com.ua</a>
+<!--==============================footer=================================-->
+  
+</body>
+</html>
